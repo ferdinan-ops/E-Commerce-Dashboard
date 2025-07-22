f58bfae39ba38e05891069fe0e46009b4e1e0c06
Ferdinan Imanuel Tumanggor <84588706+ferdinan-ops@users.noreply.github.com>
2025-07-22 10:14:25 +0700

    CHORE: Update default option for React Query

--- src/components/providers/QueryProvider.tsx
+++ src/components/providers/QueryProvider.tsx
@@ -2,7 +2,17 @@ import * as React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = React.useState(() => new QueryClient())
+  const [queryClient] = React.useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        retry: 1,
+        staleTime: 5 * 60 * 1000
+      }
+    }
+  }))
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
